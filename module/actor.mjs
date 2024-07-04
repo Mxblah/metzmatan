af1598b06b122ba010d04cd2d3de9cdeffb98e79
Mxblah <shaun@clockworksun.com>
2024-07-03 22:46:13 -0400

    Floor derived values

--- module/actor.mjs
+++ module/actor.mjs
@@ -10,15 +10,15 @@ export class MzMaActor extends Actor {
 
         // Resources //
         // Clamp and handle HP based on formula
-        hp.max = body / 5 + hp.levelBonus
+        hp.max = Math.floor(body / 5 + hp.levelBonus)
         hp.value = Math.clamp(hp.value, hp.min, hp.max)
         hp.levelBonus = Math.clamp(hp.levelBonus, Math.floor(level / 4) + 1, (Math.floor(level / 4) + 1) * 6)
 
         // Clamp and handle MP based on formula
-        mp.max = ( mind + soul ) / 5 - 4
+        mp.max = Math.floor((mind + soul) / 5 - 4)
         mp.value = Math.clamp(mp.value, mp.min, mp.max)
 
         // Handle DB based on formula
-        this.system.attributes.dodgeBonus = body / 5 - 4
+        this.system.attributes.dodgeBonus = Math.floor(body / 5 - 4)
     }
 }
