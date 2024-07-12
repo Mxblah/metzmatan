a1d2dedcb5cff20c7d99ad7d833b65753ebf8a6a
Mxblah <shaun@clockworksun.com>
2024-07-11 23:42:19 -0400

    just forgot an await; it's working now still in progress though; only logging so far

--- module/helpers/degree-of-success.mjs
+++ module/helpers/degree-of-success.mjs
@@ -1,6 +1,6 @@
 export async function getDOS(roll) {
     // Evaluate the roll first so we can see what we're working with
-    roll.evaluate()
+    await roll.evaluate()
 
     // debug
     console.log(roll.terms)
@@ -9,10 +9,18 @@ export async function getDOS(roll) {
     console.log(roll.total)
 
     // Find the d100 and get its result
-    const d100Object = roll.terms.find((term) => term._faces === 100)
-    console.log(d100Object)
-    const d100Result = d100Object.results[0].result
+    const d100Result = roll.terms.find((term) => term._faces === 100).results[0].result
     console.log(d100Result)
+    const firstDigit = Math.floor(d100Result / 10) % 10 // Extra % 10 so 100 -> 0
+    const secondDigit = d100Result % 10
+    console.log(`first: ${firstDigit}, second: ${secondDigit}`)
+
+    // todo: determine if brutal by adding up all the bonuses to see if > 100 or < 0
+
+    // Determine if critical
+    if (firstDigit === secondDigit) {
+        console.log("Roll is critical!")
+    }
 
     return 'nothing yet'
 }
