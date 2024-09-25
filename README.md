833a95c7440d335f338c83dc8e02c4ea391e68ed
Mxblah <shaun@clockworksun.com>
2024-09-24 22:37:25 -0400

    Bump version for release process testing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Metzmatan's Mark 0.1.2
+# Metzmatan's Mark 0.1.3
 
 [![Build release artifacts](https://github.com/Mxblah/metzmatan/actions/workflows/release.yml/badge.svg)](https://github.com/Mxblah/metzmatan/actions/workflows/release.yml)
 
--- scripts/build-rules.rb
+++ scripts/build-rules.rb
@@ -1,7 +1,10 @@
 require 'asciidoctor-pdf'
 
+# Initial variable declarations
 version = ''
 sourcePath = "#{__dir__}/../rules/main.adoc"
+
+# Walk the main asciidoc file to find the version number (it's very near the top so this should be fast)
 File.open(sourcePath) do |file|
     file.each_line do |line|
         if (line.match?(/:revnumber:/))
@@ -11,6 +14,7 @@ File.open(sourcePath) do |file|
 end
 outputPath = "#{__dir__}/../rules/metzmatan-test-rulebook-#{version}.pdf"
 
+# Build the actual pdf. "Unsafe" actually just means "resolve include directives" instead of just leaving them as links
 puts "Building rulebook pdf from primary source file '#{sourcePath}' with version #{version}"
 Asciidoctor.convert_file sourcePath, backend: 'pdf', safe: :unsafe, to_file: outputPath
 
